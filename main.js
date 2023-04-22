91bad1f98a257e4c7554d389614fee8255d72045
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2023-04-22 18:59:55 +0200

    Ejercicio

---
+++ main.js
@@ -0,0 +1,29 @@
+
+let seriesPending = ['Breaking Bug', 'Game of Drones', 'Boolean 99', 'Arrested Developer'];
+let seriesWatching=[];
+
+seriesPending.push('The Last Of Us');
+
+for (let i=0; i<seriesPending.length; i++){
+    if (seriesPending[i]=== 'Boolean 99'){
+    console.log('Found it!');
+    }
+    console.log(seriesPending[i]);
+}
+
+let removedSerie=seriesPending.shift();
+seriesWatching.push(removedSerie);
+console.log(seriesPending.length);
+console.log(seriesWatching.length);
+
+for (let j=0; j===4;j++){
+    seriesWatching.push(seriesPending.shift());
+}
+console.log(seriesPending.length);
+console.log(seriesWatching.length);
+
+
+seriesWatching.splice(0, seriesWatching.length);
+
+console.log(seriesPending.length);
+console.log(seriesWatching.length);
